5c4fe93cfa15f88babb81b069a01e443c6687a26
ugobitterly <ugobitterly@localhost>
2021-12-26 11:36:47 +0100

    Update Etudiant.java

--- src/main/java/com/example/application/views/helloworld/Etudiant.java
+++ src/main/java/com/example/application/views/helloworld/Etudiant.java
@@ -1,18 +1,12 @@
 package com.example.application.views.helloworld;
 ;
 import com.example.application.views.MainLayout;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
-import com.vaadin.flow.component.tabs.TabsVariant;
 /**
  *
  * @author ugobitterly
@@ -49,7 +43,7 @@ public class Etudiant extends VerticalLayout {
 
 		if (tab.equals(accueil)) {
 			content.add(new Paragraph("Pour chacun des 3 groupes de modules suivants vous allez devoir choisir quels sont les modules qui vous intéressent."),
-                        new Paragraph("Veuillez en choisir entre 3 et 5 pour chaque groupe."),
+                        new Paragraph("Veuillez en choisir 3 pour chaque groupe."),
                         new Paragraph("Choisissez bien et vérifiez avant de valider vos choix, pas de modifications possibles !"));
 		} else if (tab.equals(groupe1)) {
 			content.add(new Paragraph("Voici les modules proposés dans le groupe 1"),new Paragraph("Sélectionnez ceux de votre choix"));
