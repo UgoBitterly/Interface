71f1eec93d76b792ccbdf3cda3801ffa8983b692
ugobitterly <ugobitterly@localhost>
2021-12-30 16:05:10 +0100

    Update Admin.java

--- src/main/java/com/example/application/views/helloworld/Admin.java
+++ src/main/java/com/example/application/views/helloworld/Admin.java
@@ -1,6 +1,5 @@
 package com.example.application.views.helloworld;
 
-import java.util.Locale;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -86,7 +85,8 @@ public class Admin extends VerticalLayout {
             
 	content.removeAll();
         //ici on défini le contenu à afficher pour chaque "tab" de l'interface
-        //semestre
+        //Semestre
+        //creation d'un semestre
         TextField numero = new TextField("Numéro");
         TextField annee = new TextField("Année");
         Button creersemestre = new Button("Créer", event -> {
@@ -121,7 +121,16 @@ public class Admin extends VerticalLayout {
         HorizontalLayout buttonsemestre = new HorizontalLayout(creersemestre,rsemestre);
         buttonsemestre.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
         
-        //groupe (de modules)
+        //supprimer un semestre
+        TextField semestreasupprimer = new TextField("Numéro du semestre");
+        Button effacersemestre = new Button("Effacer");
+        effacersemestre.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        HorizontalLayout supprimersemestre = new HorizontalLayout(semestreasupprimer,effacersemestre);
+        supprimersemestre.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+        
+        
+        //Groupe (de modules)
+        //creation d'un groupe de module
         TextField creneau = new TextField("Créneau horaire");
         Button creergroupe = new Button("Créer", event -> {
             String creneau1 = creneau.getValue();
@@ -145,16 +154,27 @@ public class Admin extends VerticalLayout {
         HorizontalLayout buttongroupe = new HorizontalLayout(creergroupe,rgroupe);
         buttongroupe.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
         
-        //module
+        //supprimer un groupe de module
+        TextField groupeasupprimer = new TextField("Créneau du groupe");
+        Button effacergroupe = new Button("Effacer");
+        effacergroupe.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        HorizontalLayout supprimergroupe = new HorizontalLayout(groupeasupprimer,effacergroupe);
+        supprimergroupe.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+        
+        
+        //Module
+        //création d'un module
         TextField nommodule = new TextField("Nom");
         IntegerField nbmax = new IntegerField("Capacité maximale");
-        IntegerField nbmin = new IntegerField("Capacité minimale");
         nbmax.setPlaceholder("Entier max");
+        IntegerField nbmin = new IntegerField("Capacité minimale");
         nbmin.setPlaceholder("Entier min");
+        TextField choixgroupemodule = new TextField("Groupe du module");
         Button creermodule = new Button("Créer" , event -> {
             String nom = nommodule.getValue();
             Integer nbmax1 = nbmax.getValue();
             Integer nbmin1 = nbmin.getValue();
+            String choixgroupe = choixgroupemodule.getValue();
             try{
             int numbermax = nbmax1;
             try{
@@ -179,15 +199,25 @@ public class Admin extends VerticalLayout {
         Button rmodule = new Button("Réinitialiser", event -> {
                 nommodule.setValue("");
                 nbmax.setValue(0);
-                nbmin.setValue(0);     
+                nbmin.setValue(0);
+                choixgroupemodule.setValue("");
         });
         rmodule.addThemeVariants(ButtonVariant.LUMO_ERROR);
-        HorizontalLayout creationmodule = new HorizontalLayout(nommodule,nbmax,nbmin);
+        HorizontalLayout creationmodule = new HorizontalLayout(nommodule,nbmax,nbmin,choixgroupemodule);
         creationmodule.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
         HorizontalLayout buttonmodule = new HorizontalLayout(creermodule,rmodule);
         buttonmodule.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
         
-        //etudiant
+        //supprimer un module
+        TextField moduleasupprimer = new TextField("Module à supprimer");
+        Button effacermodule = new Button("Effacer");
+        effacermodule.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        HorizontalLayout supprimermodule = new HorizontalLayout(moduleasupprimer,effacermodule);
+        supprimermodule.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+        
+        
+        //Etudiant
+        //creation d'un etudiant
         TextField nom = new TextField("Nom");
         TextField prenom = new TextField("Prénom");
         EmailField mail = new EmailField("Adresse mail INSA");
@@ -198,8 +228,6 @@ public class Admin extends VerticalLayout {
             String nomminuscule = nomM.toLowerCase();
             mail.setValue(prenomminuscule+"."+nomminuscule+"@insa-strasbourg.fr");
         });
-        //mail.setPlaceholder("utilisateur@insa-strasbourg.fr");
-        //mail.setErrorMessage("Veuillez entrer une adresse mail INSA");
         TextField mdp = new TextField("Mot de Passe");
         DatePicker datenaissance = new DatePicker("Date de naissance");
         Button creeretudiant = new Button("Créer", event -> { 
@@ -231,17 +259,40 @@ public class Admin extends VerticalLayout {
         HorizontalLayout buttonetudiant = new HorizontalLayout(creeretudiant,retudiant);
         buttonetudiant.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
         
+        //supprimer un etudiant
+        TextField etudiantasupprimer = new TextField("Etudiant à supprimer");
+        Button effaceretudiant = new Button("Effacer");
+        effaceretudiant.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        HorizontalLayout supprimeretudiant = new HorizontalLayout(etudiantasupprimer,effaceretudiant);
+        supprimeretudiant.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+        
         //ici on affiche le menu correspondant en fonction du choix de création
 	if (tab.equals(accueil)) {
-            content.add(new Paragraph("Veuillez choisir ce que vous voulez créer en cliquant sur l'icone correspondant"));
+            content.add(new Paragraph("Veuillez choisir ce que vous voulez créer ou supprimer en cliquant sur l'icone correspondant"));
+            
         } else if (tab.equals(semestre)) {
-            content.add(new Paragraph("Veuillez entrer les attributs du nouveau semestre"),creationsemestre,buttonsemestre);
+            content.add(new Paragraph("Veuillez entrer les attributs du nouveau semestre"),creationsemestre,buttonsemestre,
+                    new Paragraph(""),new Paragraph(""),//je mets ça pour sauter deux lignes
+                    new Paragraph("Si vous souhaitez supprimer un semestre, veuillez rentrer son numéro ci-dessous :"),
+                    supprimersemestre);
+            
 	} else if (tab.equals(groupe)) {
-            content.add(new Paragraph("Veuillez entrer les attributs du nouveau groupe"),creationgroupe,buttongroupe);
+            content.add(new Paragraph("Veuillez entrer les attributs du nouveau groupe"),creationgroupe,buttongroupe,
+                    new Paragraph(""),new Paragraph(""), 
+                    new Paragraph("Si vous souhaitez supprimer un groupe de module, veuillez rentrer son créneau ci-dessous :"),
+                    supprimergroupe);
+            
 	} else if (tab.equals(module)){
-            content.add(new Paragraph("Veuillez entrer les attributs du nouveau module"),creationmodule,buttonmodule);
+            content.add(new Paragraph("Veuillez entrer les attributs du nouveau module"),creationmodule,buttonmodule,
+                    new Paragraph(""),new Paragraph(""),
+                    new Paragraph("Si vous souhaitez supprimer un module, veuillez rentrer son nom ci-dessous :"),
+                    supprimermodule);
+            
 	} else {
-            content.add(new Paragraph("Veuillez entrer les attributs du nouvel étudiant"),creationetudiant,buttonetudiant);
+            content.add(new Paragraph("Veuillez entrer les attributs du nouvel étudiant"),creationetudiant,buttonetudiant,
+                    new Paragraph(""),new Paragraph(""),
+                    new Paragraph("Si vous souhaitez supprimer un étudiant, veuillez rentrer son nom ci-dessous :"),
+                    supprimeretudiant);
         }
     }catch (Exception ex) {
             System.out.println("Probleme : " + ex);
